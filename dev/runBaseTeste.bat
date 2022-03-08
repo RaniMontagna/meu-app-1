89f083f27197e594f758a22660e3be9bf1446ba5
Ranielli Montagna <182746@upf.br>
2022-03-08 20:25:30 -0300

    feat: primeiro commit

---
+++ dev/runBaseTeste.bat
@@ -0,0 +1,7 @@
+::#region
+mongod --port 27018 --dbpath C:\Users\ranie\Documents\topicos1\mongo\baseTeste --auth
+::#endregion
+
+rem mongo --port 27018
+rem mongo -u "182746" -p "182746" --authenticationDatabase "teste" --port 27018
+rem mongodb://182746:182746@localhost:27018/teste?authSource=teste
